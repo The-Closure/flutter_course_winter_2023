e7f4d8107963840fd0a959bb4b9293c010059fa7
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-15 16:55:00 +0200

    init point 1

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,5 +1,44 @@
-import 'package:flutter_course/flutter_course.dart' as flutter_course;
+import 'package:test/test.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${flutter_course.calculate()}!');
+void main() {
+  List<num> mylist = [1, 3, 4, 5, 6];
+
+  // ! mylist func
+  Map mymap = {'name': 'ahmad', 'age': 18, 'gander': 'male'};
+  mylist.add(4);
+  print(mylist);
+  mylist.asMap();
+  print(mylist);
+
+  // ! mymap func
+
+  mymap.forEach(
+    (key, value) => print(value),
+  );
+
+  String x = mymap.toString();
+
+  print(x.toUpperCase());
+
+  mymap.updateAll((key, value) => key = 'size');
+  print(mymap);
+  // print(mymap['name']);
+
+
+// ? test the switch case 
+  // switch (mylist.length) {
+  //   case 3:
+  //     print(mylist[3]);
+  //     break;
+  //   case 4:
+  //     print(mylist[4]);
+  //     break;
+  //   case 5:
+  //     print('hello world');
+  //     break;
+  //   default:
+  //     print(mylist[4]);
+  // }
+
+  
 }
--- flutter_course/lib/flutter_course.dart
+++ flutter_course/lib/flutter_course.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:math';
+
+Future<int> calculate() async {
+  Duration(seconds: 3);
+  int x = 5;
+  Duration(seconds: 2);
+  return Future.delayed(Duration(seconds: 5), () {
+    int a = Random().nextInt(10);
+    return a;
+  });
 }
