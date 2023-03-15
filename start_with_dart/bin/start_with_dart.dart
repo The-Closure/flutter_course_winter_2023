7bf35aa5719bffa26ef3d5c4a7fd159ce1fa135d
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-15 11:57:50 +0200

    init

---
+++ start_with_dart/bin/start_with_dart.dart
@@ -0,0 +1,5 @@
+import 'package:start_with_dart/start_with_dart.dart' as start_with_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${start_with_dart.calculate()}!');
+}
---
+++ start_with_dart/test/start_with_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:start_with_dart/start_with_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
