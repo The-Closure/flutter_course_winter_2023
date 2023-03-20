af2a1c4221c0487ae920359591f10591bd5e69c7
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-20 09:05:59 +0200

    example

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,44 +1,11 @@
 import 'package:test/test.dart';
 
 void main() {
-  List<num> mylist = [1, 3, 4, 5, 6];
+  int add(int x, {int a = 4, required int b}) {
+    return a + b + x;
+  }
 
-  // ! mylist func
-  Map mymap = {'name': 'ahmad', 'age': 18, 'gander': 'male'};
-  mylist.add(4);
-  print(mylist);
-  mylist.asMap();
-  print(mylist);
+  int x = 4;
 
-  // ! mymap func
-
-  mymap.forEach(
-    (key, value) => print(value),
-  );
-
-  String x = mymap.toString();
-
-  print(x.toUpperCase());
-
-  mymap.updateAll((key, value) => key = 'size');
-  print(mymap);
-  // print(mymap['name']);
-
-
-// ? test the switch case 
-  // switch (mylist.length) {
-  //   case 3:
-  //     print(mylist[3]);
-  //     break;
-  //   case 4:
-  //     print(mylist[4]);
-  //     break;
-  //   case 5:
-  //     print('hello world');
-  //     break;
-  //   default:
-  //     print(mylist[4]);
-  // }
-
-  
+  x == 5 ? print(add(x, a: 0, b: 4)) : print("Not Five");
 }
