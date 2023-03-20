fdc935deeeab741b0da985614fb229a97fe53ef5
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-20 09:08:10 +0200

    OOP simple ex

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,11 +1,67 @@
-import 'package:test/test.dart';
+void main(){
+  Point origin = Point.origin();
+  origin.position();
+  origin.verticalMovement(10);
+  origin.position();
+  origin.horizentalMovement(4,'left');
+  origin.position();
+}
 
-void main() {
-  int add(int x, {int a = 4, required int b}) {
-    return a + b + x;
+class Point{
+  int? x;
+  int y = 0;
+  
+  void _moveRight(int steps)
+  {
+      x = (x ?? 0) + steps; 
+  }  
+  
+  void _moveLeft(int steps){
+    x = (x ?? 0) - steps;
   }
-
-  int x = 4;
-
-  x == 5 ? print(add(x, a: 0, b: 4)) : print("Not Five");
+  
+  void _moveUp(int steps){
+    y = y + steps;
+  }
+  
+  void _moveDown(int steps){
+    y = y - steps;
+  }
+  
+  void position(){
+    print('current position is x : $x , y : $y');
+  }
+  
+  void horizentalMovement(int steps,[String direction = 'right']){
+    if(direction == 'right'){
+      _moveRight(steps);
+    }else if(direction == 'left'){
+      _moveLeft(steps);
+    }else {
+      throw Exception('not supported operation');
+    }
+  }
+  
+  void verticalMovement(int steps,[String direction = 'up']){
+    if(direction == 'up'){
+      _moveUp(steps);
+    }else if(direction == 'down'){
+      _moveDown(steps);
+    }else {
+      throw Exception('not supported operation');
+    }
+  }
+  
+  //Point(this.x, this.y);
+  
+  //Point(this.y,[this.x]);
+  
+  Point({required this.y,this.x});
+  
+  Point.origin(){
+    x = 0;
+    y = 0;
+  }
+  
+  
 }
