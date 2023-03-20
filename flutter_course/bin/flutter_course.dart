ff3c8be84475981b370b69392c1db1a300a5d8a7
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-20 16:16:21 +0200

    add

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,67 +1,67 @@
-void main(){
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+void main() {
   Point origin = Point.origin();
   origin.position();
   origin.verticalMovement(10);
   origin.position();
-  origin.horizentalMovement(4,'left');
+  origin.horizentalMovement(4, 'left');
   origin.position();
+
 }
 
-class Point{
+class Point {
   int? x;
-  int y = 0;
-  
-  void _moveRight(int steps)
-  {
-      x = (x ?? 0) + steps; 
-  }  
-  
-  void _moveLeft(int steps){
+  int y = 9;
+
+
+  void _moveRight(int steps) {
+    x = (x ?? 0) + steps;
+  }
+
+  void _moveLeft(int steps) {
     x = (x ?? 0) - steps;
   }
-  
-  void _moveUp(int steps){
+
+  void _moveUp(int steps) {
     y = y + steps;
   }
-  
-  void _moveDown(int steps){
+
+  void _moveDown(int steps) {
     y = y - steps;
   }
-  
-  void position(){
+
+  void position() {
     print('current position is x : $x , y : $y');
   }
-  
-  void horizentalMovement(int steps,[String direction = 'right']){
-    if(direction == 'right'){
+
+  void horizentalMovement(int steps, [String direction = 'right']) {
+    if (direction == 'right') {
       _moveRight(steps);
-    }else if(direction == 'left'){
+    } else if (direction == 'left') {
       _moveLeft(steps);
-    }else {
+    } else {
       throw Exception('not supported operation');
     }
   }
-  
-  void verticalMovement(int steps,[String direction = 'up']){
-    if(direction == 'up'){
+
+  void verticalMovement(int steps, [String direction = 'up']) {
+    if (direction == 'up') {
       _moveUp(steps);
-    }else if(direction == 'down'){
+    } else if (direction == 'down') {
       _moveDown(steps);
-    }else {
+    } else {
       throw Exception('not supported operation');
     }
   }
-  
-  //Point(this.x, this.y);
-  
-  //Point(this.y,[this.x]);
-  
-  Point({required this.y,this.x});
-  
-  Point.origin(){
+
+
+  Point({required this.y, this.x});
+
+  Point.origin() {
     x = 0;
     y = 0;
   }
-  
-  
 }
