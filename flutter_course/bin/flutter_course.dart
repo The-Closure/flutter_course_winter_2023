744e95c8c6fa5e402e4e9502897e6191ef06a64c
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-22 17:36:30 +0200

    init

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,40 +1,92 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 
-void main() {
-  Cars car1 = Cars(name: 'BMW', price: 200);
+import 'package:flutter_course/flutter_course.dart';
 
-  car1.initprice(90.0);
+void main() async {
+  Dio dio = Dio();
 
-  print(car1.fetchprice());
-  
+  Response result =
+      await dio.get('http://jsonplaceholder.typicode.com/posts/1');
+
+  Model data = Model.fromMap(result.data);
+
+  print(data.userId);
+  print(data.id);
+
+  print(data.title);
+  print(data.body);
 }
 
-class MyHanlder extends FormatException {
-  @override
-  String get message => 'This is an Error from MyHanlder';
-
-  String get message1 => '';
-}
-
-class Cars {
-  String name;
-  double price;
-  Cars({
-    required this.name,
-    required this.price,
+class Model {
+  int userId;
+  int id;
+  String title;
+  String body;
+  Model({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
   });
 
-  double fetchprice() {
-    price = price * 0.6;
-
-    return price;
+  Model copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    String? body,
+  }) {
+    return Model(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+    );
   }
 
-  double initprice(value) {
-    price = value;
-    return price;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Model.fromMap(Map<String, dynamic> map) {
+    return Model(
+      userId: map['userId'] as int,
+      id: map['id'] as int,
+      title: map['title'] as String,
+      body: map['body'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Model.fromJson(String source) =>
+      Model.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Model(userId: $userId, id: $id, title: $title, body: $body)';
+  }
+
+  @override
+  bool operator ==(covariant Model other) {
+    if (identical(this, other)) return true;
+
+    return other.userId == userId &&
+        other.id == id &&
+        other.title == title &&
+        other.body == body;
+  }
+
+  @override
+  int get hashCode {
+    return userId.hashCode ^ id.hashCode ^ title.hashCode ^ body.hashCode;
   }
 }
--- flutter_course/lib/flutter_course.dart
+++ flutter_course/lib/flutter_course.dart
@@ -1,11 +1,21 @@
 import 'dart:math';
 
 Future<int> calculate() async {
-  Duration(seconds: 3);
-  int x = 5;
-  Duration(seconds: 2);
-  return Future.delayed(Duration(seconds: 5), () {
-    int a = Random().nextInt(10);
-    return a;
+
+  
+  Future.delayed(Duration(seconds: 10), () {
+    int x = 4;
+    print(x);
+    return x;
   });
+
+  return 7;
+
+  // Duration(seconds: 3);
+  // int x = 5;
+  // Duration(seconds: 2);
+  // return Future.delayed(Duration(seconds: 5), () {
+  //   int a = Random().nextInt(10);
+  //   return a;
+  // });
 }
