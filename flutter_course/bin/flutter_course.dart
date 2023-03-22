79262146c9fdd5926d796fc1350cefd4715fefe3
Abdalaziztll <abdalaziztll992010@gmail.com>
2023-03-22 16:28:05 +0200

    edit

--- flutter_course/bin/flutter_course.dart
+++ flutter_course/bin/flutter_course.dart
@@ -1,24 +1,40 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 import 'package:test/test.dart';
 
 void main() {
-  MyHanlder Tester = MyHanlder();
+  Cars car1 = Cars(name: 'BMW', price: 200);
 
-  try {
-    int x = 9;
-    var y = 9 / 0;
-  } catch (exception) {
-    throw Tester.message;
-  }
+  car1.initprice(90.0);
+
+  print(car1.fetchprice());
+  
 }
 
 class MyHanlder extends FormatException {
-
   @override
   String get message => 'This is an Error from MyHanlder';
 
   String get message1 => '';
+}
 
+class Cars {
+  String name;
+  double price;
+  Cars({
+    required this.name,
+    required this.price,
+  });
 
+  double fetchprice() {
+    price = price * 0.6;
+
+    return price;
+  }
+
+  double initprice(value) {
+    price = value;
+    return price;
+  }
 }
